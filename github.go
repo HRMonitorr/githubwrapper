5ef4097d5e66f43181597b2f4c812910b92f0251
rofinafiin <rofinafiisr@gmail.com>
2023-11-13 13:41:40 +0000

    Upload file to repository

--- github.go
+++ github.go
@@ -1,8 +1,13 @@
 package githubwrapper
 
 import (
+	"bufio"
 	"context"
+	"fmt"
 	"github.com/google/go-github/v56/github"
+	"io"
+	"log"
+	"os"
 )
 
 func MakeClient(personalToken string) *github.Client {
@@ -56,6 +61,39 @@ func ListRepositoriesOnlydDetail(ctx context.Context, personalToken, OrgName str
 			Homepage: v.Homepage,
 		}
 		dest = append(dest, data)
+	}
+	return
+}
+
+func UploadFileToRepository(ctx context.Context, PersonalToken, Reponame, OwnerName, Pathfile string) (response *github.RepositoryContentResponse, err error) {
+	file, err := os.Open(Pathfile)
+	if err != nil {
+		log.Fatalf("Gagal open file %s", err.Error())
+		return
+	}
+	defer file.Close()
+	stat, err := file.Stat()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	bs := make([]byte, stat.Size())
+	_, err = bufio.NewReader(file).Read(bs)
+	if err != nil && err != io.EOF {
+		fmt.Println(err)
+		return
+	}
+	opts := &github.RepositoryContentFileOptions{
+		Message:   github.String("This is my commit message"),
+		Content:   bs,
+		Branch:    github.String("master"),
+		Committer: &github.CommitAuthor{Name: github.String("FirstName LastName"), Email: github.String("user@example.com")},
+	}
+	response, _, err = MakeClient(PersonalToken).Repositories.CreateFile(ctx, OwnerName, Reponame, Pathfile, opts)
+	if err != nil {
+		fmt.Printf("%+v", err.Error())
+		return
 	}
 	return
 }
